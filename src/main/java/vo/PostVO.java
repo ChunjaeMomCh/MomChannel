07c77baf6a30b49495d0fe7ecbfbeda2583b715c
Lzynee <raina2zen90@gmail.com>
2023-11-26 00:27:20 +0900

    PostVO의 alias 설정

--- src/main/java/vo/PostVO.java
+++ src/main/java/vo/PostVO.java
@@ -3,11 +3,13 @@ package vo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.ibatis.type.Alias;
 
 import java.util.Date;
 
 @Getter @Setter
 @ToString
+@Alias("pvo")
 public class PostVO {
 
     private int postNo;  // 게시글 번호
