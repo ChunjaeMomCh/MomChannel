6ded47731e010a4e7373984016a8f481f4100354
Lzynee <raina2zen90@gmail.com>
2023-11-28 13:23:20 +0900

    [#208] Fix(feat/post): 상세 페이지 리디렉션 오류 수정

--- src/main/java/controller/PostViewController.java
+++ src/main/java/controller/PostViewController.java
@@ -21,9 +21,8 @@ import java.util.Map;
 public class PostViewController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
         // 게시물 불러오기
         PostDAO dao = new PostDAO();  // DAO 객체 생성
         // 게시물의 일련번호를 매개변수로 받아 조회수를 증가시킨다.
@@ -34,7 +33,20 @@ public class PostViewController extends HttpServlet {
         PostVO vo = dao.viewPost(postNo);
 
         // 줄바꿈 처리 : 일반 텍스트 문서의 줄바꿈 문자(\r\n) => html이 인식하는 줄바꿈 태그(<br>)
-        vo.setPostContent(vo.getPostContent().replaceAll("\r\n", "<br>"));
+        String postContent = vo.getPostContent();
+
+
+        try {
+
+            if (postContent != null || !postContent.equals("")) {
+
+                vo.setPostContent(postContent.replaceAll("\r\n", "<br>"));
+            }
+
+        } catch (NullPointerException e) {
+            vo.setPostContent(postContent);
+
+        }
 
         // 첨부 파일 확장자 추출 및 이미지 타입 확인
         // 첨부 파일이 이미지라면 <img> 태그로 상세보기 화면에 보여준다.
@@ -58,5 +70,7 @@ public class PostViewController extends HttpServlet {
         req.setAttribute("vo", vo);
         req.setAttribute("isImage", isImage);
         req.getRequestDispatcher("/post/PostView.jsp").forward(req, resp);
-    }
-}
+
+    }  // service()
+
+}  // class
